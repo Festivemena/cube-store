4153ba4af41b8303b85cccde8f1ef3a0d70bb9d1
Festivemena <88979259+Festivemena@users.noreply.github.com>
2023-03-29 04:49:20 +0100

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,57 @@
+import Shirt1 from '../assets/1.jpg'
+import Shirt2 from '../assets/2.jpg'
+import Shirt3 from '../assets/3.jpg'
+import Shirt4 from '../assets/4.jpg'
+import Shirt5 from '../assets/5.jpg'
+import Shirt6 from '../assets/6.jpg'
+import Shirt7 from '../assets/7.jpg'
+import Shirt8 from '../assets/8.jpg'
+import Shirt9 from '../assets/9.jpg'
+import Shirt10 from '../assets/10.jpg'
+import Shirt11 from '../assets/11.jpg'
+import Shirt12 from '../assets/12.jpg'
+import Shirt13 from '../assets/13.jpg'
+import Shirt14 from '../assets/14.jpg'
+import Shirt15 from '../assets/15.jpg'
+import Shirt16 from '../assets/16.jpg'
+import Shirt17 from '../assets/17.jpg'
+import Shirt18 from '../assets/18.jpg'
+import Shirt19 from '../assets/19.jpg'
+import Girl1 from '../assets/IMG.jpg'
+import Man1 from '../assets/man1.png'
+import Woman1 from '../assets/woman1.png'
+import Woman2 from '../assets/woman2.png'
+import Woman3 from '../assets/woman3.png'
+import Woman4 from '../assets/woman4.png'
+import Woman5 from '../assets/woman5.png'
+import Obi from '../assets/obi.png'
+
+export {
+    Man1,
+    Woman1,
+    Woman2,
+    Woman3,
+    Woman4,
+    Woman5,
+    Obi,
+    Shirt1,
+    Shirt2,
+    Shirt3,
+    Shirt4,
+    Shirt5,
+    Shirt6,
+    Shirt7,
+    Shirt8, 
+    Shirt9, 
+    Shirt10,
+    Shirt11,
+    Shirt12,
+    Shirt13,
+    Shirt14,
+    Shirt15,
+    Shirt16,
+    Shirt17,
+    Shirt18,
+    Shirt19,
+    Girl1,
+};
